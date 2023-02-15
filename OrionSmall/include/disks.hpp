c764b943950f70cb17962fae8a9320e81e0388f6
will grellier <grellierw@gmail.com>
2023-02-14 23:00:51 -0800

    Initial commit
    Contains code to for chassis ez library and running intakes

---
+++ OrionSmall/include/disks.hpp
@@ -0,0 +1,21 @@
+#ifndef _disks_HPP_
+#define _disks_HPP_
+#include "api.h"
+#include "EZ-Template/PID.hpp"
+
+//Cata
+void pidCata();
+
+
+
+//Helper
+
+void setCata(int motorPower);
+
+
+
+
+
+
+
+#endif
